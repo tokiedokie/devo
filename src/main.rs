b6b9b1326d64a77e3b62125bacfbe4ad3b7ec631
tokiedokie <thetokiedokie@gmail.com>
2020-08-27 15:24:49 +0900

    create help() version() deno_run()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::env;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    match args.get(1) {
+        Some(command) => match command.as_str() {
+            "help" => help(),
+            "--help" => help(),
+            "-h" => help(),
+            "--version" => version(),
+            _ => deno_run(&args.split_at(1).1.to_vec()),
+        },
+        _ => deno_run(&Vec::new()),
+    }
+}
+
+fn help() {
+    deno_run(&vec!["help".to_owned()])
+}
+
+fn version() {
+    let devo_version = env!("CARGO_PKG_VERSION");
+    println!("devo {}", devo_version);
+    deno_run(&vec!["--version".to_owned()]);
+}
+
+fn deno_run(args: &Vec<String>) {
+    Command::new("deno")
+        .args(args)
+        .spawn()
+        .expect("deno failed to start");
 }
